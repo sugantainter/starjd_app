81ffbfda52efb1026ef10ac1c31f492ca7c8737f
starjd <suganta1@gmail.com>
2026-04-27 16:15:26 +0530

    refactor: remove Sign in with Apple integration and clear iOS development team configuration

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../main.dart';
 import '../services/auth_service.dart';
@@ -155,16 +154,7 @@ class _LoginScreenState extends State<LoginScreen> {
             );
           }
           return;
-          }
         }
-      } else if (provider == 'apple') {
-        final appleCredential = await SignInWithApple.getAppleIDCredential(
-          scopes: [
-            AppleIDAuthorizationScopes.email,
-            AppleIDAuthorizationScopes.fullName,
-          ],
-        );
-        token = appleCredential.identityToken;
       }
       
       if (token != null) {
@@ -421,14 +411,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ],
                 ),
-                
-                if (Theme.of(context).platform == TargetPlatform.iOS) ...[
-                  const SizedBox(height: 16),
-                  SignInWithAppleButton(
-                    onPressed: () => _handleSocialLogin('apple'),
-                    style: isDark ? SignInWithAppleButtonStyle.white : SignInWithAppleButtonStyle.black,
-                  ),
-                ],
                 
                 const SizedBox(height: 40),
                 
